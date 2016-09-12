0b0aef31a86b31f94bc2b3e49a0e369567c19d7d
nzbradley7 <bdc.vanuatu@gmail.com>
2016-09-12 18:30:27 +1200

    Cleaned up the code

--- listings.php
+++ listings.php
@@ -6,30 +6,30 @@ require 'config.php';
 $sql = "SELECT * FROM jobs ORDER BY jobID DESC";
 $result = $conn->query($sql);
 while($row = $result->fetch_assoc()) {
-  echo("<div class='well well-sm'>
-    <div data-toggle='collapse' data-target='#" . $row['jobID'] . "' style='padding: 1%;'>
+  ?><div class='well well-sm'>
+    <div data-toggle='collapse' data-target='#<?php echo($row['jobID']) ?>' style='padding: 1%;'>
       <h3 class='text-center'>
-        ". $row['name'] . "<i class='fa fa-chevron-circle-down' aria-hidden='true' style='float:right;'></i>
+        <?php echo($row['name']) ?><i class='fa fa-chevron-circle-down' aria-hidden='true' style='float:right;'></i>
       </h3>
     </div>
-    <div class='collapse job-info' id='" . $row['jobID'] . "'>
+    <div class='collapse job-info' id='<?php echo($row['jobID']) ?>'>
       <div id='description'>
         <h4>Description:</h4>
-        <p class='text-justify'>". $row['description'] . "</p>
+        <p class='text-justify'><?php echo($row['description']) ?></p>
       </div>
       <br>
       <div class='row'>
         <div class='col-sm-6'>
-          <h4>Email:</h4><p class='contact'>". $row['email'] . "</p>
-          <h4>Phone:</h4><p class='contact'>". $row['phone'] . "</p>
+          <h4>Email:</h4><p class='contact'><?php echo($row['email']) ?></p>
+          <h4>Phone:</h4><p class='contact'><?php echo($row['phone']) ?></p>
         </div>
         <div class='col-sm-6'>
-          <h4>Type:</h4><p class='contact'>". $row['type'] . "</p>
-          <h4>Location:</h4><p class='contact'>" . $row['location'] . "</p>
+          <h4>Type:</h4><p class='contact'><?php echo($row['type']) ?></p>
+          <h4>Location:</h4><p class='contact'><?php echo($row['location']) ?></p>
         </div>
       </div>
     </div>
-  </div>");
+  </div><?php
   }
 
 $conn->close();
